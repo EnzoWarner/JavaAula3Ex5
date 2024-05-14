5f47855b0cba839df12bb44f85c16932e98e5399
EnzoWarner <enzo_warner@hotmail.com>
2024-05-14 02:20:20 -0300

    first commit

---
+++ Java Aula 3 Ex 5.java
@@ -0,0 +1,7 @@
+public class EX3_5 {
+    public static void main (String[] args) {
+    short nascimento = 2002;
+    short atual = 2023;
+    System.out.println("Sua idade e: " +(atual - nascimento));
+}
+}
